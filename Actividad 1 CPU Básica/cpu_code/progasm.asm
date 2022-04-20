ae5082b4dd4a1a2b0cab4e639ff7c714993e182f
alu0101278919 <alu0101278919@ull.edu.es>
2022-04-20 10:05:59 +0100

    trying timer with simple assembly program.

--- Actividad 1 CPU Básica/cpu_code/progasm.asm
+++ Actividad 1 CPU Básica/cpu_code/progasm.asm
@@ -1,30 +1,6 @@
-li R1, 65533  
-li R2, 65535
-li R3, 65532 
-li R5, 65534 
-li R7, 65531
-li R8, 1
+li R1, 1
+li R2, 65534
 
 start:
-load R4, R1
-subi R9, R4, 1 
-jz readstart
-subi R9, R4, 2
-jz stop 
-subi R9, R4, 4
-jz showdata
-j start 
-
-readstart: 
-li R14, 0 
-li R15, 1
+addi R0, R0, 0
 j start
-
-stop:
-li R14, 0
-li R15, 0
-j start
-
-showdata:
-li R15, 0
-li R14, 1
